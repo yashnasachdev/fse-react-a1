9f8ffdaf7f4ff41ec34f6ca8079d8dc249769303
Yashna <78277101+yashnasachdev@users.noreply.github.com>
2022-02-06 14:57:17 -0500

    Delete src/components/models directory

--- src/components/models/Location.ts
+++
@@ -1,4 +0,0 @@
-export default class Location {
-   public latitude: number = 0.0;
-   public longitude: number = 0.0;
-};
--- src/components/models/Topic.ts
+++
@@ -1,4 +0,0 @@
-export default class Topic {
-    private topic: string = '';
-}
-
