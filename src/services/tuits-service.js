24cbad6e098606ab709fb12a1fca0e1f557c5dd5
yashnasachdev <yashna.sach@gmail.com>
2022-05-05 09:44:57 -0400

    a4 updates

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,31 +1,42 @@
 import axios from "axios";
 
-//const TUITS_API = "https://silly-einstein-c7beac.netlify.app/api/tuits";
-//const USERS_API = "https://silly-einstein-c7beac.netlify.app/api/users";
-const TUITS_API = "http://localhost:4000/api/tuits";
-const USERS_API = "http://localhost:4000/api/users";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const TUITS_API = `${BASE_URL}/tuits`;
+const USERS_API = `${BASE_URL}/users`;
+// http://localhost:4000
+// const TUITS_API = "https://tuiter-a3-jw.herokuapp.com/tuits";
+// const USERS_API = "https://tuiter-a3-jw.herokuapp.com/users";
 
+const api = axios.create({
+    withCredentials: true
+});
 
 export const findAllTuits = () =>
-    axios.get(TUITS_API)
+    api.get(TUITS_API)
         .then(response => response.data);
 
 export const findTuitById = (tid) =>
-    axios.get(`${TUITS_API}/${tid}`)
+    api.get(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
-export const findTuitByUser = (uid) =>
-    axios.get(`${USERS_API}/${uid}/tuits`)
+// retrieve tuits posted by user
+export const findTuitsByUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/tuits`)
         .then(response => response.data);
 
+// post tuit by user
 export const createTuit = (uid, tuit) =>
-    axios.post(`${USERS_API}/${uid}/tuits`, tuit)
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
         .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
-    axios.post(`${TUITS_API}/${tid}`, tuit)
+    api.post(`${TUITS_API}/${tid}`, tuit)
         .then(response => response.data);
 
 export const deleteTuit = (tid) =>
-    axios.delete(`${TUITS_API}/${tid}`)
+    api.delete(`${TUITS_API}/${tid}`)
+        .then(response => response.data);
+
+export const deleteTuitByContent = (tuit) =>
+    api.delete(`${TUITS_API}/content/${tuit}/delete`)
         .then(response => response.data);
